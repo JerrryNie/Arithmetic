215ad2733688bb06577cc23f55c2776773533493
JerrryNie <815398117@qq.com>
2019-01-15 22:41:07 +0800

    实现倒计时功能

--- gui-app/gui-app/Form1.Designer.cs
+++ gui-app/gui-app/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.StartButton = new System.Windows.Forms.Button();
             this.TimeCnt = new System.Windows.Forms.TextBox();
             this.CountDown = new System.Windows.Forms.TextBox();
@@ -37,41 +38,52 @@
             this.QuesNum = new System.Windows.Forms.TextBox();
             this.ConfirmAnsButton = new System.Windows.Forms.Button();
             this.FinishButton = new System.Windows.Forms.Button();
+            this.TextReadOnly2 = new System.Windows.Forms.TextBox();
+            this.TextReadOnly3 = new System.Windows.Forms.TextBox();
+            this.TextReadOnly4 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // StartButton
             // 
-            this.StartButton.Location = new System.Drawing.Point(686, 24);
+            this.StartButton.Location = new System.Drawing.Point(688, 50);
             this.StartButton.Name = "StartButton";
-            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.Size = new System.Drawing.Size(100, 36);
             this.StartButton.TabIndex = 0;
             this.StartButton.Text = "开始答题";
             this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
             // 
             // TimeCnt
             // 
-            this.TimeCnt.Location = new System.Drawing.Point(31, 75);
+            this.TimeCnt.Location = new System.Drawing.Point(545, 61);
             this.TimeCnt.Name = "TimeCnt";
+            this.TimeCnt.ReadOnly = true;
             this.TimeCnt.Size = new System.Drawing.Size(100, 25);
             this.TimeCnt.TabIndex = 1;
             // 
             // CountDown
             // 
-            this.CountDown.Location = new System.Drawing.Point(558, 22);
+            this.CountDown.Location = new System.Drawing.Point(69, 61);
             this.CountDown.Name = "CountDown";
+            this.CountDown.ReadOnly = true;
             this.CountDown.Size = new System.Drawing.Size(100, 25);
             this.CountDown.TabIndex = 2;
+            this.CountDown.TextChanged += new System.EventHandler(this.CountDown_TextChanged);
             // 
             // QuestionText
             // 
             this.QuestionText.Location = new System.Drawing.Point(2, 223);
             this.QuestionText.Name = "QuestionText";
+            this.QuestionText.ReadOnly = true;
             this.QuestionText.Size = new System.Drawing.Size(414, 25);
             this.QuestionText.TabIndex = 3;
+            this.QuestionText.TextChanged += new System.EventHandler(this.QuestionText_TextChanged);
             // 
             // AnsText
             // 
-            this.AnsText.Location = new System.Drawing.Point(545, 223);
+            this.AnsText.Location = new System.Drawing.Point(463, 223);
             this.AnsText.Name = "AnsText";
             this.AnsText.Size = new System.Drawing.Size(100, 25);
             this.AnsText.TabIndex = 4;
@@ -98,12 +110,13 @@
             // 
             // ConfirmAnsButton
             // 
-            this.ConfirmAnsButton.Location = new System.Drawing.Point(686, 279);
+            this.ConfirmAnsButton.Location = new System.Drawing.Point(613, 226);
             this.ConfirmAnsButton.Name = "ConfirmAnsButton";
-            this.ConfirmAnsButton.Size = new System.Drawing.Size(75, 23);
+            this.ConfirmAnsButton.Size = new System.Drawing.Size(100, 36);
             this.ConfirmAnsButton.TabIndex = 7;
             this.ConfirmAnsButton.Text = "确认答案";
             this.ConfirmAnsButton.UseVisualStyleBackColor = true;
+            this.ConfirmAnsButton.Click += new System.EventHandler(this.ConfirmAnsButton_Click);
             // 
             // FinishButton
             // 
@@ -113,12 +126,62 @@
             this.FinishButton.TabIndex = 8;
             this.FinishButton.Text = "答题结束";
             this.FinishButton.UseVisualStyleBackColor = true;
+            this.FinishButton.Click += new System.EventHandler(this.FinishButton_Click);
+            // 
+            // TextReadOnly2
+            // 
+            this.TextReadOnly2.BackColor = System.Drawing.SystemColors.Control;
+            this.TextReadOnly2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextReadOnly2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.TextReadOnly2.Location = new System.Drawing.Point(12, 27);
+            this.TextReadOnly2.Name = "TextReadOnly2";
+            this.TextReadOnly2.ReadOnly = true;
+            this.TextReadOnly2.Size = new System.Drawing.Size(51, 18);
+            this.TextReadOnly2.TabIndex = 9;
+            this.TextReadOnly2.Text = "倒计时：";
+            // 
+            // TextReadOnly3
+            // 
+            this.TextReadOnly3.BackColor = System.Drawing.SystemColors.Control;
+            this.TextReadOnly3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextReadOnly3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.TextReadOnly3.Location = new System.Drawing.Point(458, 27);
+            this.TextReadOnly3.Multiline = true;
+            this.TextReadOnly3.Name = "TextReadOnly3";
+            this.TextReadOnly3.ReadOnly = true;
+            this.TextReadOnly3.Size = new System.Drawing.Size(105, 25);
+            this.TextReadOnly3.TabIndex = 10;
+            this.TextReadOnly3.Text = "答题已用时：";
+            // 
+            // TextReadOnly4
+            // 
+            this.TextReadOnly4.BackColor = System.Drawing.SystemColors.Control;
+            this.TextReadOnly4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextReadOnly4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.TextReadOnly4.Location = new System.Drawing.Point(438, 226);
+            this.TextReadOnly4.Multiline = true;
+            this.TextReadOnly4.Name = "TextReadOnly4";
+            this.TextReadOnly4.ReadOnly = true;
+            this.TextReadOnly4.Size = new System.Drawing.Size(18, 22);
+            this.TextReadOnly4.TabIndex = 11;
+            this.TextReadOnly4.Text = "=";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TextReadOnly4);
+            this.Controls.Add(this.TextReadOnly3);
+            this.Controls.Add(this.TextReadOnly2);
             this.Controls.Add(this.FinishButton);
             this.Controls.Add(this.ConfirmAnsButton);
             this.Controls.Add(this.QuesNum);
@@ -146,6 +209,11 @@
         private System.Windows.Forms.TextBox QuesNum;
         private System.Windows.Forms.Button ConfirmAnsButton;
         private System.Windows.Forms.Button FinishButton;
+        private System.Windows.Forms.TextBox TextReadOnly2;
+        private System.Windows.Forms.TextBox TextReadOnly3;
+        private System.Windows.Forms.TextBox TextReadOnly4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
     }
 }
 
--- gui-app/gui-app/Form1.cs
+++ gui-app/gui-app/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace gui_app
 {
@@ -15,6 +16,128 @@ namespace gui_app
         public Form1()
         {
             InitializeComponent();
+            String Str = "Hp * hp + hp ** hp";
+            QuestionText.Text = Str;
+            //计时器属性初始化
+            timer1.Interval = 1000;
+            timer2.Interval = 1000;
+            //t.Elapsed += new System.Timers.ElapsedEventHandler(Theout);
+            //到达时间的时候执行事件； 
+            //t.AutoReset = true;
+
+            //设置是执行一次（false）还是一直执行(true)；
+        }
+
+        private const int MaxThinkTime = 10;
+        private int Timecnt = 0;//用来记录做题总时间
+        /*public void Theout(object source, System.Timers.ElapsedEventArgs e)
+        {//用于计时器触发的函数，在这里用来记录流逝的秒，并进行显示
+            Timecnt++;
+            using (BackgroundWorker bw = new BackgroundWorker())
+            {
+                bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bw_RunWorkerCompleted);
+                bw.DoWork += new DoWorkEventHandler(bw_DoWork);
+                bw.RunWorkerAsync(Timecnt.ToString());
+            }
+            //.Text =  "m";
+        }
+        void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // 这里是后台线程， 是在另一个线程上完成的
+            // 这里是真正做事的工作线程
+            // 可以在这里做一些费时的，复杂的操作
+            Thread.Sleep(5000);
+            e.Result = e.Argument + "m";
+        }
+        void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //这时后台线程已经完成，并返回了主线程，所以可以直接使用UI控件了 
+            this.TimeCnt.Text = e.Result.ToString();
+        }
+        System.Timers.Timer t = new System.Timers.Timer(1000);
+        //实例化Timer类，设置间隔时间为1000毫秒；
+        */
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            //Step1
+            //点击“开始答题”之后，从这里可以将生成n道题的信息传出去，并同时从Controller接收
+            //题目和相应的答案，并将答案显示在QuestionText这个控件中
+            //与此同时，开始显示倒计时(TimeCnt)和已用时间这两个信息(CountDown)
+
+            //这是一个测试用的显示文本
+            //String Str = "Hp * hp + hp ** hp";
+            //开始答题后，计时开始
+            //t.Start();
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            //需要调用 timer.Start()或者timer.Enabled = true来启动它， timer.Start()的内部原理还是设置timer.Enabled = true;
+
+
+        }
+
+        private void QuestionText_TextChanged(object sender, EventArgs e)
+        {
+            //Step2.1
+            //这个控件中显示相应的题目，并不允许用户对这个控件的属性进行修改，
+            String Str = "Hp * hp + hp ** hp";
+            QuestionText.Text = Str;
+        }
+
+        private void CountDown_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FinishButton_Click(object sender, EventArgs e)
+        {
+            //处理计时器
+           // t.Stop();//计时器触发时间关闭
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            Timecnt = 0;
+            CountDownCnt = MaxThinkTime;
+            //TimeCnt.Text = "";
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {//用来修改正向计时
+            Timecnt++;
+            String Str = Timecnt.ToString();
+            TimeCnt.Text = Str + "秒";
+        }
+        int CountDownCnt = MaxThinkTime;
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            CountDownCnt--;
+            String Str = CountDownCnt.ToString();
+            CountDown.Text = Str + "秒";
+            if(CountDownCnt <= 0)
+            {//当倒计时结束后，给Controller发出错误信息，并初始化倒计时器
+
+                timer2.Enabled = false;//当倒计时结束后 ，计时器的计时将暂停，此函数将不再被调用
+                const int ErrorAns = -1;//设置一个恒为错误值的数字，用来表示当倒计时结束时的答案输出
+                bool status = Send(ErrorAns);//发送错误答案给Controller
+                status = false;//将本次答题结果恒置为false, 表示错误
+                //MessageBox.Show("");
+                DialogResult result = MessageBox.Show("本题的答题时间已到！", "Warning", MessageBoxButtons.OKCancel);
+                if (result == DialogResult.OK)
+                {
+                    timer2.Enabled = true;//开启计时器的计时功能
+                }
+                //初始化倒计时
+                CountDownCnt = MaxThinkTime;
+
+            }
+        }
+        private bool Send(int Ans)
+        {//暂时没有加入Controller，恒返回1，表示答案正确
+            return true;
+        }
+
+        private void ConfirmAnsButton_Click(object sender, EventArgs e)
+        {//用户单击“确认答案”按键后，触发此函数
+
         }
     }
 }
